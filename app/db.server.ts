0d907a4ef75cb7d0334ae4bef37135064f3b1aa3
Dennis <dennisjr89@gmail.com>
2026-02-27 23:03:44 -0500

    :(

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,10 +1,15 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "~/lib/generated/prisma/client.js";
-
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-const db = new PrismaClient({ adapter });
-
-export { db };
+import { PrismaPg } from "@prisma/adapter-pg";
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default prisma;
