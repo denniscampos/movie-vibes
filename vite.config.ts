a20ed33861d57bc0428f6f178d6e46c80e720742
Dennis <dennisjr89@gmail.com>
2026-02-27 23:23:25 -0500

    idk

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["@prisma/client", /^@prisma\/client\/.*/],
-    },
-  },
   server: {
     port: 3000,
   },
